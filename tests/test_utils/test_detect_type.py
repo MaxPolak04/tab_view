30003365af46a2f4dc8c85bc09dcdc12b243e747
Maksymilian Polak <max.polak04@gmail.com>
2026-01-28 12:43:46 +0100

    Update test_utils/

--- tests/test_utils/test_detect_type.py
+++ tests/test_utils/test_detect_type.py
@@ -4,15 +4,45 @@ from tab_view.utils import detect_type
 
 @pytest.mark.parametrize(
     'filename',
-    ['photo.jpg', 'photo.jpeg', 'photo.png', 'photo.gif']
+    ['photo.jpg', 'photo.jpeg', 'photo.png', 'photo.gif', 'PHOTO.JPG', 'Image.PnG']
 )
-def test_detect_type_returns_image_for_image_extensions(filename):
+def test_detect_type_returns_image(filename):
+    """
+    Checks image extensions, including case variations.
+    """
     assert detect_type(filename) == 'image'
 
 
 @pytest.mark.parametrize(
-        'filename',
-        ['movie.mp4', 'clip.webm', 'presentation.mov']
+    'filename',
+    ['movie.mp4', 'clip.webm', 'presentation.mov', 'ARCHIVE.MP4']
 )
-def test_detect_type_returns_video_for_non_image_extensions(filename):
+def test_detect_type_returns_video(filename):
+    """
+    Checks video extensions.
+    """
     assert detect_type(filename) == 'video'
+
+
+@pytest.mark.parametrize(
+    'filename',
+    ['my.super.photo.jpg', 'data.backup.tar.gz.mp4']
+)
+def test_detect_type_handles_multiple_dots(filename):
+    """
+    Ensures that the function only checks the last extension after the last period.
+    """
+    if filename.endswith('jpg'):
+        assert detect_type(filename) == 'image'
+    else:
+        assert detect_type(filename) == 'video'
+
+
+def test_detect_type_defaults_to_video_for_unknown():
+    """
+    Tests the current application logic: anything that is not an image
+    is treated as a video (even PDF or TXT).
+    """
+    assert detect_type('document.pdf') == 'video'
+    assert detect_type('readme.txt') == 'video'
+    
--- tests/test_utils/test_str_to_bool.py
+++ tests/test_utils/test_str_to_bool.py
@@ -4,15 +4,39 @@ from tab_view.utils import str_to_bool
 
 @pytest.mark.parametrize(
     "value",
-    ["true", "True", "TRUE", "1", "yes", "Yes", "YES"]
+    [
+        "true", "True", "TRUE",
+        "1",
+        "yes", "Yes", "YES",
+        1,
+        True
+    ]
 )
-def test_str_to_bool_returns_true_for_truthy_values(value):
+def test_str_to_bool_returns_true(value):
+    """
+    Test inputs that should evaluate to True.
+    Includes mixed types (int, bool) to verify type casting.
+    """
     assert str_to_bool(value) is True
 
 
 @pytest.mark.parametrize(
     "value",
-    ["false", "False", "0", "no", "None", "", None]
+    [
+        "false", "False",
+        "0",
+        "no",
+        "random_text",
+        "",
+        None,
+        0,
+        False
+    ]
 )
-def test_str_to_bool_returns_false_for_falsy_values(value):
+def test_str_to_bool_returns_false(value):
+    """
+    Test inputs that should evaluate to False.
+    Anything not explicitly in the 'true' list should return False.
+    """
     assert str_to_bool(value) is False
+    
